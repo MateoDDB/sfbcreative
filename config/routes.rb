eeea13eba206f23d5854e783a15c9f74b16cc7ce
Mateo <mateo-ceron@hotmail.fr>
2020-06-09 16:54:42 +0200

    pages about us

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   resources :contacts
   get "pages/clients"
   get "pages/services"
+  get "pages/about"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
