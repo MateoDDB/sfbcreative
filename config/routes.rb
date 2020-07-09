193f62fea168b8b27bcbddf240e0406491785b8f
Mateo <mateo-ceron@hotmail.fr>
2020-07-09 15:56:03 +0200

    Page package

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,6 @@ Rails.application.routes.draw do
   get "pages/clients"
   get "pages/services"
   get "pages/about"
+  get "pages/offering"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
